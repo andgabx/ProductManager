85c10415395dcf71829dc9a4c8f4c6f4f3265459
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2024-12-26 10:31:02 -0300

    refactor: add UpsertDialog

--- app/products/_components/add-product-button.tsx
+++ app/products/_components/add-product-button.tsx
@@ -1,67 +1,22 @@
 "use client";
 
-import { Loader2Icon, PlusIcon } from "lucide-react";
 import {
   Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/app/_components/ui/dialog";
 import { Button } from "@/app/_components/ui/button";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/app/_components/ui/form";
-import { Input } from "@/app/_components/ui/input";
-import { NumericFormat } from "react-number-format";
-import { createProduct } from "../_actions/create-product";
+import { PlusIcon } from "lucide-react";
 import { useState } from "react";
-import { toast } from "sonner";
 
-const formSchema = z.object({
-  name: z.string().min(1, { message: "Nome é obrigatório" }).trim().max(30), // ver a documentacao do zod pela mor
-  price: z.number().min(0.01, { message: "Preço é obrigatório" }),
-  stock: z.coerce
-    .number()
-    .positive()
-    .min(0, { message: "Estoque é obrigatório" }),
-});
+import UpsertDialog from "./upsert-dialog";
 
-type FormSchema = z.infer<typeof formSchema>;
+
+
 
 const AddProductButton = () => {
-  const [DialogOpen, setDialogOpen] = useState(false);
-  const form = useForm<FormSchema>({
-    shouldUnregister: true,
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      price: 0,
-      stock: 1,
-    },
-  });
+        const [DialogOpen, setDialogOpen] = useState(false);
 
-  const onSubmit = async (data: FormSchema) => {
-    try {
-      await createProduct(data);
-      setDialogOpen(false);
-      toast.success("Produto criado com sucesso!");
-    } catch (error) {
-      toast.error("Erro ao criar produto");
-      console.log(error);
-    }
-  };
+
 
   return (
     <Dialog open={DialogOpen} onOpenChange={setDialogOpen}>
@@ -71,103 +26,9 @@ const AddProductButton = () => {
           <span className="text-sm text-white">Novo Produto</span>
         </Button>
       </DialogTrigger>
+      <UpsertDialog onSucess={() => setDialogOpen(false)} />
 
-      <DialogContent className="bg-background">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <DialogHeader className="space-y-4">
-              <DialogTitle>Criar Produto</DialogTitle>
-              <DialogDescription className="py-2">
-                Insira os dados do produto abaixo
-              </DialogDescription>
-            </DialogHeader>
-            <div className="space-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome do Produto</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="border-primary-dark"
-                        placeholder="Digite o nome do produto"
-                        {...field}
-                      />
-                    </FormControl>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="price"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Preço do Produto</FormLabel>
-                    <FormControl>
-                      <NumericFormat
-                        thousandSeparator="."
-                        decimalSeparator=","
-                        fixedDecimalScale={true}
-                        decimalScale={2}
-                        prefix="R$"
-                        allowNegative={false}
-                        customInput={Input}
-                        className="border-primary-dark"
-                        placeholder="Digite o preço do produto"
-                        onValueChange={(value) => {
-                          field.onChange(value.floatValue);
-                        }}
-                        {...field}
-                        onChange={() => {}}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="stock"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Estoque do Produto</FormLabel>
-                    <FormControl>
-                      <NumericFormat
-                        allowNegative={false}
-                        customInput={Input}
-                        className="border-primary-dark"
-                        placeholder="Digite o estoque do produto"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type="reset" variant="outline">
-                    Cancelar
-                  </Button>
-                </DialogClose>
-                <Button
-                  disabled={form.formState.isSubmitting}
-                  type="submit"
-                  className="bg-primary text-white"
-                >
-                  {form.formState.isSubmitting && (
-                    <Loader2Icon className="animate-spin" />
-                  )}
-                  Criar Produto
-                </Button>
-              </DialogFooter>
-            </div>
-          </form>
-        </Form>
-      </DialogContent>
+      
     </Dialog>
   );
 };
